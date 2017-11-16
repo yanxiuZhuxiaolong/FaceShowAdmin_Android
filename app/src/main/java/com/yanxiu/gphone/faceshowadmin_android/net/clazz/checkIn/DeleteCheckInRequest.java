b60222c67c9ac753273f1b9855652146b2d932dd
frcwindy <frcwindy@gmail.com>
2017-11-16 12:20:27 +0800

    add 删除签到

---
+++ app/src/main/java/com/yanxiu/gphone/faceshowadmin_android/net/clazz/checkIn/DeleteCheckInRequest.java
@@ -0,0 +1,10 @@
+package com.yanxiu.gphone.faceshowadmin_android.net.clazz.checkIn;
+
+import com.yanxiu.gphone.faceshowadmin_android.net.base.FaceShowBaseRequest;
+
+/**
+ * Created by frc on 17-11-16.
+ */
+
+public class DeleteCheckInRequest extends FaceShowBaseRequest {
+}
---
+++ app/src/main/java/com/yanxiu/gphone/faceshowadmin_android/net/clazz/checkIn/DeleteCheckInResponse.java
@@ -0,0 +1,10 @@
+package com.yanxiu.gphone.faceshowadmin_android.net.clazz.checkIn;
+
+import com.yanxiu.gphone.faceshowadmin_android.net.base.FaceShowBaseResponse;
+
+/**
+ * Created by frc on 17-11-16.
+ */
+
+public class DeleteCheckInResponse extends FaceShowBaseResponse {
+}
